b24992601067b20aa4dcda289b72335477dce77c
adrnel <adriannelson1989@gmail.com>
2024-05-12 17:15:29 +0100

    Add more recipes

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,24 +3,37 @@ import React from 'react';
 const Footer = () => (
   <footer className="bg-gray-900 text-white p-4">
     <div className="container mx-auto px-4 flex flex-wrap justify-between items-center">
-      <div className="w-full md:w-auto mb-4 md:mb-0">
+      <div className="w-full md:w-auto mb-4 md:mb-0 text-center">
         <p>Contact us: +123 456 7890 | contact@quickrecipes.com</p>
-        <div className="social-icons">
-          {/* Icons for social media */}
-        </div>
+        <div className="social-icons">{/* Icons for social media */}</div>
       </div>
       <div className="w-full md:w-auto">
         <form className="flex flex-col md:flex-row items-center">
-          <label htmlFor="email-signup" className="mr-2">Sign up for updates:</label>
-          <input type="email" id="email-signup" placeholder="Your email" className="flex-1 m-2 p-2 rounded"/>
-          <button type="submit" className="m-2">Subscribe</button>
+          <label htmlFor="email-signup" className="mr-2">
+            Sign up for updates:
+          </label>
+          <input
+            type="email"
+            id="email-signup"
+            placeholder="Your email"
+            className="flex-1 m-2 p-2 rounded"
+          />
+          <button type="submit" className="m-2">
+            Subscribe
+          </button>
         </form>
       </div>
     </div>
     <div className="container mx-auto px-4 flex flex-wrap justify-between items-center mt-4">
-      <a href="#privacy" className="mb-2 md:mb-0">Privacy Policy</a>
-      <a href="#terms" className="mb-2 md:mb-0">Terms of Use</a>
-      <p>© 2024 Quick Easy Recipes. All rights reserved.</p>
+      <a href="#privacy" className="mb-2 md:mb-0">
+        Privacy Policy
+      </a>
+      <a href="#terms" className="mb-2 md:mb-0">
+        Terms of Use
+      </a>
+      <p className="text-center w-full md:w-auto">
+        © 2024 Quick Easy Recipes. All rights reserved.
+      </p>
     </div>
   </footer>
 );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Image from 'next/image';
 import Slider from 'react-slick';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Link from 'next/link';
 
 const settings = {
   dots: true,
@@ -11,23 +11,23 @@ const settings = {
   speed: 500,
   slidesToShow: 3,
   slidesToScroll: 1,
-  centerPadding: "60px",
+  centerPadding: '60px',
   responsive: [
     {
       breakpoint: 768,
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1,
-      }
-    }
-  ]
+      },
+    },
+  ],
 };
 
 const HomePage = () => {
   return (
     // <div className="relative h-screen w-full">
-    //   <Image 
-    //     src="/spaghetti.png" 
+    //   <Image
+    //     src="/spaghetti.png"
     //     alt="Delicious Spaghetti"
     //     fill
     //     style={{ objectFit: 'cover', objectPosition: 'center' }}
@@ -37,47 +37,76 @@ const HomePage = () => {
     //   </div>
     // </div>
     <main className="relative">
-      <div className="bg-hero-pattern bg-cover bg-center h-screen w-full flex items-center justify-center text-white text-shadow" style={{ backgroundImage: 'url(/chili_con_carne.png)' }}>
-        <h1 className="text-6xl font-bold text-white max-w-2xl mx-auto px-4 my-8 text-center">Discover Delicious Dishes</h1>
+      <div
+        className="bg-hero-pattern bg-cover bg-center h-screen w-full flex items-center justify-center text-white text-shadow"
+        style={{ backgroundImage: 'url(/chili_con_carne.png)' }}
+      >
+        <h1 className="text-6xl font-bold text-white max-w-2xl mx-auto px-4 my-8 text-center">
+          Discover Delicious Dishes
+        </h1>
       </div>
       <section className="container mx-auto px-4 py-8">
         <h2 className="text-2xl font-bold mb-6">Top Recipes</h2>
         <Slider {...settings}>
           <div className="relative">
-            <img src="/tikka_masala.png" alt="Tika Masala" />
-            <div className="absolute inset-0 flex items-center justify-center">
-              <p className="text-white text-2xl font-bold text-shadow">Tika Masala</p>
-            </div>
+            <Link href="/recipes/1">
+              <img src="/tikka_masala.png" alt="Tika Masala" />
+              <div className="absolute inset-0 flex items-center justify-center">
+                <p className="text-white text-2xl font-bold text-shadow">
+                  Tika Masala
+                </p>
+              </div>
+            </Link>
           </div>
           <div className="relative">
-            <img src="/shepards_pie.png" alt="Shepards Pie" />
-            <div className="absolute inset-0 flex items-center justify-center">
-              <p className="text-white text-2xl font-bold text-shadow">Shepards Pie</p>
-            </div>
+            <Link href="/recipes/2">
+              <img src="/shepards_pie.png" alt="Shepards Pie" />
+              <div className="absolute inset-0 flex items-center justify-center">
+                <p className="text-white text-2xl font-bold text-shadow">
+                  Shepards Pie
+                </p>
+              </div>
+            </Link>
           </div>
           <div className="relative">
-            <img src="/beef_stroganoff.png" alt="Beef Stroganoff" />
-            <div className="absolute inset-0 flex items-center justify-center">
-              <p className="text-white text-2xl font-bold text-shadow">Beef Stroganoff</p>
-            </div>
+            <Link href="/recipes/3">
+              <img src="/beef_stroganoff.png" alt="Beef Stroganoff" />
+              <div className="absolute inset-0 flex items-center justify-center">
+                <p className="text-white text-2xl font-bold text-shadow">
+                  Beef Stroganoff
+                </p>
+              </div>
+            </Link>
           </div>
           <div className="relative">
-            <img src="/chili_con_carne.png" alt="Recipe 2" />
-            <div className="absolute inset-0 flex items-center justify-center">
-              <p className="text-white text-2xl font-bold text-shadow">Chili Con Carne</p>
-            </div>
+            <Link href="/recipes/3">
+              <img src="/chili_con_carne.png" alt="Chili Con Carne" />
+              <div className="absolute inset-0 flex items-center justify-center">
+                <p className="text-white text-2xl font-bold text-shadow">
+                  Chili Con Carne
+                </p>
+              </div>
+            </Link>
           </div>
           <div className="relative">
-            <img src="/spaghetti_bolognese.png" alt="Recipe 2" />
-            <div className="absolute inset-0 flex items-center justify-center">
-              <p className="text-white text-2xl font-bold text-shadow">Spaghetti Bolognase</p>
-            </div>
+            <Link href="/recipes/5">
+              <img src="/spaghetti_bolognese.png" alt="Spaghetti Bolognese" />
+              <div className="absolute inset-0 flex items-center justify-center">
+                <p className="text-white text-2xl font-bold text-shadow">
+                  Spaghetti Bolognase
+                </p>
+              </div>
+            </Link>
           </div>
           <div className="relative">
-            <img src="/chicken_parmesan.png" alt="Chicken Parmesan" />
-            <div className="absolute inset-0 flex items-center justify-center">
-              <p className="text-white text-2xl font-bold text-shadow">Chicken Parmesan</p>
-            </div>
+            <Link href="/recipes/5">
+              <img src="/chicken_parmesan.png" alt="Chicken Parmesan" />
+              <div className="absolute inset-0 flex items-center justify-center">
+                <p className="text-white text-2xl font-bold text-shadow">
+                  Chicken Parmesan
+                </p>
+              </div>
+            </Link>
           </div>
         </Slider>
       </section>
