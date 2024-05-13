cfa1f8bedf69c7b1586efd5e3ba0511c02ca7293
adrnel <adriannelson1989@gmail.com>
2024-05-13 04:34:01 +0100

    Fix pages containing data file issue

--- src/pages/recipes/index.js
+++ src/pages/recipes/index.js
@@ -1,8 +1,23 @@
-import React from 'react';
-import RecipeCard from './RecipeCard';
-import recipes from './recipes';
+import React, { useState, useEffect } from 'react';
+import RecipeCard from '../../app/components/RecipeCard';
 
 const Recipes = () => {
+  const [recipes, setRecipes] = useState([]);  // Use an empty array as the initial state
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      const response = await fetch('/data/recipes.json');  // Adjust the path as necessary
+      const data = await response.json();
+      setRecipes(data);
+    };
+
+    fetchRecipes();
+  }, []);
+
+  if (!recipes.length) {
+    return <div>Loading...</div>;  // Loading state while data is being fetched
+  }
+
   return (
     <div className="container mx-auto px-8 py-8 max-w-6xl flex justify-center">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-2 md:gap-3">
